28d992b5a1feac77f86e335c41ce43c5213a3465
Steven Miclette <broham113@gmail.com>
2014-11-20 18:11:11 -0500

    Modified the mtg card finder to use a different search api

--- plugins/mtg.py
+++ plugins/mtg.py
@@ -2,182 +2,134 @@ import re
 
 from util import hook, http
 
+#@hook.nonick
+@hook.command
+def fchewy(inp):
+  return "fchewy"
+ 
+def stringify_card(card):
+  types = [t.capitalize() for t in card['types']]
+#  for m_type in card['types']:
+#    types.append(type.capitalize())
+  if 'subtypes' in card:
+    types.extend([t.capitalize() for t in card['subtypes']])
+    #for type in card['subtypes']:
+    #  types.append(type.capitalize())
 
+  # TODO: Deal with {2/W}-style stuff
+  card['cost'] = card['cost'].replace('{','').replace('}','')
+  card['cost'] = card['cost'] + " | "
+
+  card['text'] = card['text'].replace('\n', ' ')
+
+  e_seen = set()
+  editions = [e for e in card['editions']
+    if e['set_id'][:1] != "p"
+    and e not in e_seen
+    and not e_seen.add(e)]
+  #for e in card['editions']:
+  #  if e['set_id'][:1] != "p":
+      #addedition = edition['set_id'] + " (" + edition['rarity'][:1].upper() + ") "
+      #if addedition not in editions:
+      #  editions += addedition
+
+#  if len(editions) > 5:
+#    editions = editions[:5] + "... "
+#    if 'price' in card['editions'][0]:
+#        price = card['editions'][0]['price']
+
+  price = None
+  for edition in editions:
+    if edition['set_id'][:1] != "p" and 'price' in edition:
+      price = edition['price']
+      break
+  if price is not None:
+    prices = "L: $%s M: $%s H: $%s" % ('{:.2f}'.format(price['low']/100.), '{:.2f}'.format(price['median']/100.), '{:.2f}'.format(price['high']/100.))
+  else:
+    prices = "No prices."
+  
+  s_editions = [e['set_id'] + " (" + e['rarity'][:1].upper() + ")"
+    for e in editions[:5]]
+  if len(editions) > 5:
+    s_editions.append("...")
+  return "%s | %s | %s%s | %s| %s" % (card['name'], 
+    " ".join(types), card['cost'], card['text'], 
+    " ".join(s_editions), prices)
+#end stringify_card
+
+
+@hook.command('m')
 @hook.command
 def mtg(inp):
-    ".mtg <name> -- gets information about Magic the Gathering card <name>"
+  """!mtg [!]card - lookup information about a Magic: The Gathering 
+  card. Put a ! before the card name for an exact match."""
 
-    url = 'http://magiccards.info/query?v=card&s=cname'
-    h = http.get_html(url, q=inp)
+  #TODO: Handle other input filters
+  exact = inp[:1] == "!"
+  if exact:
+    inp = inp[1:]
+  result = http.get_json("http://api.deckbrew.com/mtg/cards", name=inp)
 
-    name = h.find('body/table/tr/td/span/a')
-    if name is None:
-        return "no cards found"
-    card = name.getparent().getparent().getparent()
+  if len(result) == 0:
+    return "No matching cards found."
 
-    type = card.find('td/p').text.replace('\n', '')
+  elif len(result) == 1:
+    return stringify_card(result[0])
 
-    # this is ugly
-    text = http.html.tostring(card.xpath("//p[@class='ctext']/b")[0])
-    text = text.replace('<br>', '$')
-    text = http.html.fromstring(text).text_content()
-    text = re.sub(r'(\w+\s*)\$+(\s*\w+)', r'\1. \2', text)
-    text = text.replace('$', ' ')
-    text = re.sub(r'\(.*?\)', '', text)  # strip parenthetical explanations
-    text = re.sub(r'\.(\S)', r'. \1', text)  # fix spacing
+  elif len(result) > 1: 
+    inp_l = inp.lower()
+    if exact == True:
+      match = filter(lambda c: c['name'].lower() == inp_l, result)
+      if match:
+        return stringify_card(match[0])
+      else:
+        #TODO: List non-matching cards?
+        return "No exact match found."
+      #end if match
 
-    printings = card.find('td/small').text_content()
-    printings = re.search(r'Editions:(.*)Languages:', printings).group(1)
-    printings = re.findall(r'\s*(.+?(?: \([^)]+\))*) \((.*?)\)',
-                           ' '.join(printings.split()))
+    else:
+      # There is no exact match
+      close_matches = filter(lambda c: inp_l in c['name'].lower(), result)
+      if len(close_matches) > 0:
+        return "Close matches: %s" % ", ".join(
+          [c['name'] for c in close_matches])
+      else:
+        return "Potential matches: %s" % ", ".join(
+          [c['name'] for c in result[:20]])
+      #end if
+    #end if exact or not
+  #end if number of results
 
-    printing_out = ', '.join('%s (%s)' % (set_abbrevs.get(x[0], x[0]),
-                                          rarity_abbrevs.get(x[1], x[1]))
-                             for x in printings)
+  return "Potential matches: %s" % ", ".join(
+    [c['name'] for c in result[:20]])
+#end mtg
 
-    name.make_links_absolute(base_url=url)
-    link = name.attrib['href']
-    name = name.text_content().strip()
-    type = type.strip()
-    text = ' '.join(text.split())
+#@hook.nonick
+@hook.regex(r'^[^:]*: ([a-zA-Z ]*) \|| Error accessing tcgplayer. \| (http.*html)')
+#@hook.regex(r'(.*)')
+def fixchewy(match,chan='',input=None):
+  if match.group(1) is not None:
+    if "Error" not in input.lastparam or "L: " in input.lastparam:
+      return
+    card = match.group(1)
+  else:
+    url = match.group(2)
+    result = http.get_html(url)
+    card = result.xpath('//title')[0].text.split("(")[0].strip()
 
-    return ' | '.join((name, type, text, printing_out, link))
+  result = http.get_json("http://api.deckbrew.com/mtg/cards", name=card)
+  if len(result) == 0:
+    return
+  for cards in result:
+    if cards['name'].lower() == card.lower():
+      card = cards
+      break
 
+  for edition in card['editions']:
+    if edition['set_id'][:1] != "p" and 'price' in edition:
+      price = edition['price']
+      break
+  if price:
+    prices = "L: $%s M: $%s H: $%s" % ('{:.2f}'.format(price['low']/100.), '{:.2f}'.format(price['median']/100.), '{:.2f}'.format(price['high']/100.))
+    return "chewy's bot sucks here are prices: %s" % prices
 
-set_abbrevs = {
-    '15th Anniversary': '15ANN',
-    'APAC Junior Series': 'AJS',
-    'Alara Reborn': 'ARB',
-    'Alliances': 'AI',
-    'Anthologies': 'AT',
-    'Antiquities': 'AQ',
-    'Apocalypse': 'AP',
-    'Arabian Nights': 'AN',
-    'Arena League': 'ARENA',
-    'Asia Pacific Land Program': 'APAC',
-    'Battle Royale': 'BR',
-    'Battle Royale Box Set': 'BRB',
-    'Beatdown': 'BTD',
-    'Beatdown Box Set': 'BTD',
-    'Betrayers of Kamigawa': 'BOK',
-    'Celebration Cards': 'UQC',
-    'Champions of Kamigawa': 'CHK',
-    'Champs': 'CP',
-    'Chronicles': 'CH',
-    'Classic Sixth Edition': '6E',
-    'Coldsnap': 'CS',
-    'Coldsnap Theme Decks': 'CSTD',
-    'Conflux': 'CFX',
-    'Core Set - Eighth Edition': '8E',
-    'Core Set - Ninth Edition': '9E',
-    'Darksteel': 'DS',
-    'Deckmasters': 'DM',
-    'Dissension': 'DI',
-    'Dragon Con': 'DRC',
-    'Duel Decks: Divine vs. Demonic': 'DVD',
-    'Duel Decks: Elves vs. Goblins': 'EVG',
-    'Duel Decks: Garruk vs. Liliana': 'GVL',
-    'Duel Decks: Jace vs. Chandra': 'JVC',
-    'Eighth Edition': '8ED',
-    'Eighth Edition Box Set': '8EB',
-    'European Land Program': 'EURO',
-    'Eventide': 'EVE',
-    'Exodus': 'EX',
-    'Fallen Empires': 'FE',
-    'Fifth Dawn': '5DN',
-    'Fifth Edition': '5E',
-    'Fourth Edition': '4E',
-    'Friday Night Magic': 'FNMP',
-    'From the Vault: Dragons': 'FVD',
-    'From the Vault: Exiled': 'FVE',
-    'Future Sight': 'FUT',
-    'Gateway': 'GRC',
-    'Grand Prix': 'GPX',
-    'Guildpact': 'GP',
-    'Guru': 'GURU',
-    'Happy Holidays': 'HHO',
-    'Homelands': 'HL',
-    'Ice Age': 'IA',
-    'Introductory Two-Player Set': 'ITP',
-    'Invasion': 'IN',
-    'Judge Gift Program': 'JR',
-    'Judgment': 'JU',
-    'Junior Series': 'JSR',
-    'Legend Membership': 'DCILM',
-    'Legends': 'LG',
-    'Legions': 'LE',
-    'Limited Edition (Alpha)': 'LEA',
-    'Limited Edition (Beta)': 'LEB',
-    'Limited Edition Alpha': 'LEA',
-    'Limited Edition Beta': 'LEB',
-    'Lorwyn': 'LW',
-    'MTGO Masters Edition': 'MED',
-    'MTGO Masters Edition II': 'ME2',
-    'MTGO Masters Edition III': 'ME3',
-    'Magic 2010': 'M10',
-    'Magic Game Day Cards': 'MGDC',
-    'Magic Player Rewards': 'MPRP',
-    'Magic Scholarship Series': 'MSS',
-    'Magic: The Gathering Launch Parties': 'MLP',
-    'Media Inserts': 'MBP',
-    'Mercadian Masques': 'MM',
-    'Mirage': 'MR',
-    'Mirrodin': 'MI',
-    'Morningtide': 'MT',
-    'Multiverse Gift Box Cards': 'MGBC',
-    'Nemesis': 'NE',
-    'Ninth Edition Box Set': '9EB',
-    'Odyssey': 'OD',
-    'Onslaught': 'ON',
-    'Planar Chaos': 'PC',
-    'Planechase': 'PCH',
-    'Planeshift': 'PS',
-    'Portal': 'PO',
-    'Portal Demogame': 'POT',
-    'Portal Second Age': 'PO2',
-    'Portal Three Kingdoms': 'P3K',
-    'Premium Deck Series: Slivers': 'PDS',
-    'Prerelease Events': 'PTC',
-    'Pro Tour': 'PRO',
-    'Prophecy': 'PR',
-    'Ravnica: City of Guilds': 'RAV',
-    'Release Events': 'REP',
-    'Revised Edition': 'RV',
-    'Saviors of Kamigawa': 'SOK',
-    'Scourge': 'SC',
-    'Seventh Edition': '7E',
-    'Shadowmoor': 'SHM',
-    'Shards of Alara': 'ALA',
-    'Starter': 'ST',
-    'Starter 1999': 'S99',
-    'Starter 2000 Box Set': 'ST2K',
-    'Stronghold': 'SH',
-    'Summer of Magic': 'SOM',
-    'Super Series': 'SUS',
-    'Tempest': 'TP',
-    'Tenth Edition': '10E',
-    'The Dark': 'DK',
-    'Time Spiral': 'TS',
-    'Time Spiral Timeshifted': 'TSTS',
-    'Torment': 'TR',
-    'Two-Headed Giant Tournament': 'THGT',
-    'Unglued': 'UG',
-    'Unhinged': 'UH',
-    'Unhinged Alternate Foils': 'UHAA',
-    'Unlimited Edition': 'UN',
-    "Urza's Destiny": 'UD',
-    "Urza's Legacy": 'UL',
-    "Urza's Saga": 'US',
-    'Visions': 'VI',
-    'Weatherlight': 'WL',
-    'Worlds': 'WRL',
-    'WotC Online Store': 'WOTC',
-    'Zendikar': 'ZEN'}
-
-rarity_abbrevs = {
-    'Land': 'L',
-    'Common': 'C',
-    'Uncommon': 'UC',
-    'Rare': 'R',
-    'Special': 'S',
-    'Mythic Rare': 'MR'}
